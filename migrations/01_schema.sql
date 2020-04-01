d76b6656ab77dc2908c34afa2ee6165d2f7b20fe
Taha Rizvi <riztaha@gmail.com>
2020-04-01 22:39:10 +0000

    Fixed typo in schema.

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE properties (
     cost_per_night INTEGER NOT NULL DEFAULT 0,
     parking_spaces INTEGER NOT NULL DEFAULT 0,
     number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
-    number_of_bedrroms INTEGER NOT NULL DEFAULT 0,
+    number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
 
     country VARCHAR(255) NOT NULL,
     street VARCHAR(255) NOT NULL,
